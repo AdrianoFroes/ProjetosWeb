bbcc88bfd145640730140725a19e1c9e52e928c5
Adriano Froes <42614927+froes0101@users.noreply.github.com>
2022-10-11 13:06:41 -0400

    Add files via upload

---
+++ meuprojetojsf/src/main/java/com/br/network/App.java
@@ -0,0 +1,9 @@
+package com.br.network;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Teste realizado com sucesso");
+
+	}
+
+}
---
+++ meuprojetojsf/src/main/java/com/br/network/ManagedBean.java
@@ -0,0 +1,5 @@
+package com.br.network;
+
+public @interface ManagedBean {
+
+}
