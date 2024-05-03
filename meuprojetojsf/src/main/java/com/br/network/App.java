07c60a70aa3c7e126e05bca936e4a4ae826be2e6
Adriano Froes <42614927+AdrianoFroes@users.noreply.github.com>
2024-05-03 11:52:09 -0300

    Delete meuprojetojsf directory

--- meuprojetojsf/src/main/java/com/br/network/App.java
+++
@@ -1,9 +0,0 @@
-package com.br.network;
-
-public class App {
-	public static void main(String[] args) {
-		System.out.println("Teste realizado com sucesso");
-
-	}
-
-}
--- meuprojetojsf/src/main/java/com/br/network/ManagedBean.java
+++
@@ -1,5 +0,0 @@
-package com.br.network;
-
-public @interface ManagedBean {
-
-}
